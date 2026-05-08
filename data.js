17923ad0d9bb7cc7d7932513de93fa60049cb634
milabrukkidul-official <milabrukkidul@gmail.com>
2026-05-08 09:17:50 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,97 @@
+const siteData = {
+  "siteTitle": "SocialHub",
+  "heroTitle": "Your <span>Social Hub</span>",
+  "heroSubtitle": "Connect with us across the digital universe. All your favorite platforms in one place.",
+  "nav": [
+    {
+      "name": "Home",
+      "link": "#"
+    },
+    {
+      "name": "Services",
+      "link": "#",
+      "dropdown": [
+        { "name": "Web Development", "link": "#web" },
+        { "name": "UI/UX Design", "link": "#design" },
+        { "name": "Consulting", "link": "#consulting" }
+      ]
+    },
+    {
+      "name": "Portfolio",
+      "link": "#",
+      "dropdown": [
+        { "name": "Projects", "link": "#projects" },
+        { "name": "Case Studies", "link": "#cases" }
+      ]
+    },
+    {
+      "name": "Contact",
+      "link": "#"
+    }
+  ],
+  "icons": [
+    {
+      "name": "GitHub",
+      "url": "https://github.com",
+      "domain": "github.com",
+      "color": "#e94560",
+      "iconClass": "fa-brands fa-github"
+    },
+    {
+      "name": "X / Twitter",
+      "url": "https://twitter.com",
+      "domain": "twitter.com",
+      "color": "#1da1f2",
+      "iconClass": "fa-brands fa-x-twitter"
+    },
+    {
+      "name": "LinkedIn",
+      "url": "https://linkedin.com",
+      "domain": "linkedin.com",
+      "color": "#0077b5",
+      "iconClass": "fa-brands fa-linkedin"
+    },
+    {
+      "name": "YouTube",
+      "url": "https://youtube.com",
+      "domain": "youtube.com",
+      "color": "#ff0000",
+      "iconClass": "fa-brands fa-youtube"
+    },
+    {
+      "name": "Instagram",
+      "url": "https://instagram.com",
+      "domain": "instagram.com",
+      "color": "#e4405f",
+      "iconClass": "fa-brands fa-instagram"
+    },
+    {
+      "name": "Discord",
+      "url": "https://discord.com",
+      "domain": "discord.com",
+      "color": "#5865f2",
+      "iconClass": "fa-brands fa-discord"
+    },
+    {
+      "name": "Spotify",
+      "url": "https://spotify.com",
+      "domain": "spotify.com",
+      "color": "#1db954",
+      "iconClass": "fa-brands fa-spotify"
+    },
+    {
+      "name": "Dribbble",
+      "url": "https://dribbble.com",
+      "domain": "dribbble.com",
+      "color": "#ea4c89",
+      "iconClass": "fa-brands fa-dribbble"
+    },
+    {
+      "name": "Figma",
+      "url": "https://figma.com",
+      "domain": "figma.com",
+      "color": "#f24e1e",
+      "iconClass": "fa-brands fa-figma"
+    }
+  ]
+};
